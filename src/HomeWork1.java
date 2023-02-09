d6937ffc6d2a582e6d0a3b433d85aada018e34ab
KatiaKost <katya-donya@mail.ru>
2023-02-09 21:20:52 +1000

    initial commit

---
+++ src/HomeWork1.java
@@ -0,0 +1,15 @@
+public class HomeWork1 {
+    public static void main(String[] args) {
+        System.out.println("Привет! Konichiwa! Ciao!");
+
+        System.out.println("1 + 2 + 3 + 4 + 5 + 6 + 7 + 8 + 9 + 10 = " + (1 + 2 + 3 + 4 + 5 + 6 + 7 + 8 + 9 + 10));
+
+        System.out.println("1 * 2 * 3 * 4 * 5 * 6 * 7 * 8 * 9 * 10 = " +  2 * 3 * 4 * 5 * 6 * 7 * 8 * 9 * 10);
+
+        System.out.println("*****  *  *    *    *****  *****  ****   *****  *   *    *  ");
+        System.out.println("*      * *    * *     *    *      *   *    *    **  *   * * ");
+        System.out.println("*****  **    *****    *    *****  ****     *    * * *  *****");
+        System.out.println("*      * *   *   *    *    *      *  *     *    *  **  *   *");
+        System.out.println("*****  *  *  *   *    *    *****  *   *  *****  *   *  *   *");
+    }
+}
